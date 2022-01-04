873034ec3e7f0e72ead8a485412405e9a9e2e4ba
Shivam-officials <suryabhardwajofficial@gmail.com>
2022-01-04 22:49:05 +0530

    wrote another instrument test for testing while making the round_up switch on

--- app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tiptime/ExampleInstrumentedTest.kt
@@ -9,26 +9,49 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.CoreMatchers.containsString
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Rule
 
+/**
+ * Class containg all the test for testing tip_options
+ */
 @RunWith(AndroidJUnit4::class)
-class CalculatorTest{
+class CalculatorTest {
 
     @get:Rule()
-            var activity = ActivityScenarioRule(MainActivity::class.java)
+    var activity = ActivityScenarioRule(MainActivity::class.java)
 
+
+    /**
+     *testing the defualt tip option by giving '50.00' as input and expeciting '10.00' as result while round up switch is off
+     */
     @Test
-    fun calculate_default_tip(){
+    fun calculate_default_tip() {
         onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00"))
 
         onView(withId(R.id.calculate_button)).perform(click())
 
         onView(withId(R.id.tip_result)).check(matches(withText(containsString("10.00"))))
 
+
+    }
+
+    /**
+     *testing the defualt tip option by giving '55.00' as input and expeciting '10.00' as result while round up switch is on
+     */
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("55"))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        onView(withId(R.id.tip_result)).check(matches(withText(containsString("10.00"))))
 
     }
 
